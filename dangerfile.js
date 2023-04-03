be7a91c4dea7e1898828d22a1b1fdffd706dd2da
Aleksandras Sukelovic <alex.shukel@gmail.com>
2023-04-03 19:53:15 +0300

    Fixed dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -1,14 +1,7 @@
 const { danger, fail } = require('danger');
-const path = require('path');
 
-const changesetsDir = path.resolve('./.changeset');
-const isChangeset = (file) => {
-	return path.dirname(path.resolve(file)) === changesetsDir && path.extname(file) === '.md';
-};
+const changesets = danger.git.fileMatch('.changeset/*.md');
 
-const createdPaths = danger.git.created_files;
-const changesets = createdPaths.filter(isChangeset);
-
-if (changesets.length === 0) {
+if (!changesets.created) {
 	fail('No changesets has been added in you pull request!');
 }
